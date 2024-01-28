9c3fad3317b7137c5e4a3158e4aa0d4ad7320e2c
Giorgio Piatti <contact@giorgiopiatti.ch>
2024-01-28 12:49:56 +0100

    update(test with select need to be plausible, otherwise is too long sampling)

--- tests/library/test_select.py
+++ tests/library/test_select.py
@@ -1,13 +1,6 @@
 from pathfinder import PathFinderModel, assistant, gen, select, user
 
 
-def test_select_basic(llm):
-    lm = llm
-    lm += " "
-    lm += select(["a", "b", "c"], name="select")
-    assert lm["select"] == "a" or lm["select"] == "b" or lm["select"] == "c"
-
-
 def test_select_tags(llm):
     lm = llm
     with user():
